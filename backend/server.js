61538799e9f0d4c0bf14e3bc6d939fbfe8e3c661
Prabhas-Kenche <prabhaskenche7145@gmail.com>
2026-06-19 20:01:50 +0530

    implement user registration and login APIs with JWT

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,30 @@ const tripRoutes = require("./routes/tripRoutes");
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/trips", tripRoutes);
+
 app.get("/", (req, res) => {
     res.send("API Running");
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/trips", tripRoutes);
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server Running ${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch(e) {
+        console.log("Failed to start server: ", e.message);
+    }
+};
+
+startServer();
