8abb42d377d971167ca73b1550a8c659565cae9d
Prabhas-Kenche <prabhaskenche7145@gmail.com>
2026-06-23 16:25:03 +0530

    Fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: [
+            "http://localhost:5000/api",
+            "https://ai-travel-planner-api-6hxk.onrender.com/api"
+        ],
     withCredentials: true,
 });
 
